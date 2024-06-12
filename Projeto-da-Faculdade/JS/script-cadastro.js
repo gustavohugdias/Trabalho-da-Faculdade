6ef39928a63a04b173568eb69b89938aa643b8cb
Gustavo Dias <65861461+gustavohugdias@users.noreply.github.com>
2024-06-12 09:00:52 -0300

    Update script-cadastro.js

--- Projeto-da-Faculdade/JS/script-cadastro.js
+++ Projeto-da-Faculdade/JS/script-cadastro.js
@@ -6,25 +6,25 @@ const carouselChildrens = [...carousel.children];
 
 let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
 
-// Get the number of cards that can fit in the carousel at once
+// Obtém o número de cards que podem caber no carrossel
 let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
 
-// Insert copies of the last few cards to beginning of carousel for infinite scrolling
+// Insere cópias dos ultimos cards para o começo do carrossel para realizar o scroll infinito.
 carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
     carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
 });
 
-// Insert copies of the first few cards to end of carousel for infinite scrolling
+// Insere cópias do primeiro card para o fim do carrossel para realizar o scroll infinito.
 carouselChildrens.slice(0, cardPerView).forEach(card => {
     carousel.insertAdjacentHTML("beforeend", card.outerHTML);
 });
 
-// Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox
+// Percorre o carrossel na posição adequada para ocultas cards duplicados.
 carousel.classList.add("no-transition");
 carousel.scrollLeft = carousel.offsetWidth;
 carousel.classList.remove("no-transition");
 
-// Add event listeners for the arrow buttons to scroll the carousel left and right
+// Adiciona eventos para os botões de seta esquerda e direita para deslocar o carrossel.
 arrowBtns.forEach(btn => {
     btn.addEventListener("click", () => {
         carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
@@ -34,14 +34,15 @@ arrowBtns.forEach(btn => {
 const dragStart = (e) => {
     isDragging = true;
     carousel.classList.add("dragging");
-    // Records the initial cursor and scroll position of the carousel
+    //     Retorna para a posição e ao scroll inicial do carrossel.
     startX = e.pageX;
     startScrollLeft = carousel.scrollLeft;
 }
 
 const dragging = (e) => {
-    if(!isDragging) return; // if isDragging is false return from here
-    // Updates the scroll position of the carousel based on the cursor movement
+    if(!isDragging) return; // se o isDragging for falso, retorna para o início.
+    
+    // Atualiza a posição do scroll do carrossel baseado no movimento do cursor.
     carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
 }
 
@@ -51,27 +52,28 @@ const dragStop = () => {
 }
 
 const infiniteScroll = () => {
-    // If the carousel is at the beginning, scroll to the end
+    // Se o carrossel estiver no início, rolará até o final.
     if(carousel.scrollLeft === 0) {
         carousel.classList.add("no-transition");
         carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
         carousel.classList.remove("no-transition");
     }
-    // If the carousel is at the end, scroll to the beginning
+    // Se o carrossel estiver no final, voltará para o início.
     else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
         carousel.classList.add("no-transition");
         carousel.scrollLeft = carousel.offsetWidth;
         carousel.classList.remove("no-transition");
     }
 
-    // Clear existing timeout & start autoplay if mouse is not hovering over carousel
+    // Limpa o tempo existente e inicia o scroll automático se o mouse não estiver sobre o carrossel.
+    
     clearTimeout(timeoutId);
     if(!wrapper.matches(":hover")) autoPlay();
 }
 
 const autoPlay = () => {
-    if(window.innerWidth < 800 || !isAutoPlay) return; // Return if window is smaller than 800 or isAutoPlay is false
-    // Autoplay the carousel after every 2500 ms
+    if(window.innerWidth < 800 || !isAutoPlay) return; // Retorna se a guia do navegador for menor que 800px ou se o "isAutoPlay" não está funcionando.
+
     timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2500);
 }
 autoPlay();
